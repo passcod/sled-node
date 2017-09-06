201d5f6e69a43847e1d6f36e7ab8f80284681c79
Félix Saparelli <felix@passcod.name>
2017-09-07 11:34:37 +1200

    Implement Tree#set

--- native/src/tree.rs
+++ native/src/tree.rs
@@ -39,6 +39,26 @@ declare_types! {
             }
         }
 
+        method set(call) {
+            let scope = call.scope;
+            let args = call.arguments;
+            let key = args.require(scope, 0)?.check::<JsString>()?.value();
+            let ints = args.require(scope, 1)?.check::<JsArray>()?.to_vec(scope)?;
+
+            let key_bytes = key.into_bytes();
+            let bytes: Vec<u8> = ints
+                .into_iter()
+                .flat_map(|i| i.downcast::<JsInteger>())
+                .map(|i| i.value() as u8)
+                .collect();
+
+            args.this(scope).grab(|wrap| {
+                wrap.0.as_ref().unwrap().set(key_bytes, bytes);
+            });
+
+            Ok(JsNull::new().as_value(scope))
+        }
+
         method toString(call) {
             let scope = call.scope;
             let string = call.arguments.this(scope)
